ab8fc6af7411ebe06d643bafe01230a0d3160764
Braden Watson <brwatson10@gmail.com>
2024-05-02 15:35:30 +0800

    Add scene loader script

---
+++ HoloLens/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByName()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
