a7462302bfd9142eb3c0890bac3e9fea6a30ffde
Braden Watson <brwatson10@gmail.com>
2024-05-21 13:13:21 +0800

    Revert "Add hololens room area target"
    This reverts commit c3d5820d90c06c0aa680108bbeaeea37770c31cf.

--- HoloLens/Assets/Scripts/SiteToggle.cs
+++ HoloLens/Assets/Scripts/SiteToggle.cs
@@ -5,29 +5,19 @@ public class SiteToggle : MonoBehaviour
     [Header("Text")]
     [SerializeField] GameObject offText;
     [SerializeField] GameObject onText;
-    
-    [Header("Toggle Status")]
-    [SerializeField] bool startToggled = false;
+
     bool isToggled = false;
 
-    [Header("Position")]
-    [SerializeField] private RectTransform tipRect;
     private readonly Vector2 _min = new(0, 0.5f);
     private readonly Vector2 _max = new(1, 0.5f);
     private readonly Vector2 _middle = new(0.5f, 0.5f);
 
+    [SerializeField] private RectTransform tipRect;
+
     void Start()
     {
-        if (startToggled)
-        {
-            isToggled = true;
-            SetOn();
-        }
-        else
-        {
-            isToggled = false;
-            SetOff();
-        }
+        isToggled = false;
+        SetOff();
     }
 
     public void Toggle()
